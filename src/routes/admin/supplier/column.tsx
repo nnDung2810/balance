aa8f194e1917391b7ddf39b2d44aa13737b2019d
SSoyBoy <114915560+SSoyBoy@users.noreply.github.com>
2023-05-08 14:17:08 +0700

    1

--- src/routes/admin/supplier/column.tsx
+++ src/routes/admin/supplier/column.tsx
@@ -93,7 +93,6 @@ export const ColumnFormSupplier= ({ t, listRole }: any) => {
             const districtId = value;
             // const districtFaca = DistrictFacade();
             form.resetFields(['district'])
-            form.setFieldValue(value);
             districtFaca.getById({districtId})
           }
         }
