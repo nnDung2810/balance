c881aa704b716fda4ba1a44acff103fd6471932c
nguyenluongnhan <110026254+nguyenluongnhan@users.noreply.github.com>
2023-05-09 11:19:28 +0700

    Update menus.tsx

--- src/layouts/admin/menus.tsx
+++ src/layouts/admin/menus.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { keyRole } from '@utils';
 import './index.less';
-import { Chart, Cog, Store, User } from '@svgs';
+import { Cart, Chart, Cog, Store, User } from '@svgs';
 
 const Layout = () => [
   {
@@ -22,6 +22,10 @@ const Layout = () => [
         permission: keyRole.P_DATA_LISTED,
       },
     ],
+  },
+  {
+    icon: <Cart className="icon-menu" />,
+    name: 'Store',
   },
   {
     icon: <Store className="icon-menu" />,
