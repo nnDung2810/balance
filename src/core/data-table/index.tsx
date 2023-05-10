45491c74e9daef943937760f10db9e78410bef67
SSoyBoy <114915560+SSoyBoy@users.noreply.github.com>
2023-05-10 08:49:17 +0700

    1

--- src/core/data-table/index.tsx
+++ src/core/data-table/index.tsx
@@ -421,7 +421,7 @@ export const DataTable = forwardRef(
               />
               {!params.fullTextSearch ? (
                 <Search
-                  className="w-5 h-5 my-1 fill-gray-500 text-lg bg-blue-50 las absolute top-1.5 left-3 z-10"
+                  className="w-5 h-5 my-1 fill-gray-500 text-lg las absolute top-1.5 left-3 z-10"
                   onClick={() => {
                     if (params.fullTextSearch) {
                       (document.getElementById(idTable.current + '_input_search') as HTMLInputElement).value = '';
